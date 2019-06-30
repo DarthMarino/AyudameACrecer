ae2d68ff0ddd5a9e8fed1d0c4a4da61dd89c66f4
Kamehametroll <34353754+Kamehametroll@users.noreply.github.com>
2019-06-30 19:12:15 -0400

    0.1
    comienzo de la app

---
+++ VistasAyudameACrecer/Cuerpo.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace VistasAyudameACrecer
+{
+    public partial class Cuerpo : Form
+    {
+        public Cuerpo()
+        {
+            InitializeComponent();
+        }
+    }
+}
